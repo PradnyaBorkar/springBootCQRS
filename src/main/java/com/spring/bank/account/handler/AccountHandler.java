fc42341ab9d3ee7e47360fdce308d083d374103e
kumb09 <pradnya.borkar@springernature.com>
2020-03-18 11:27:52 +0530

    initial spring boot configuration

--- src/main/java/com/spring/bank/account/handler/AccountHandler.java
+++ src/main/java/com/spring/bank/account/handler/AccountHandler.java
@@ -1,4 +1,27 @@
 package com.spring.bank.account.handler;
 
+import com.spring.bank.account.domain.Account;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/accounts")
 public class AccountHandler {
+
+
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Account> getAllAccounts(){
+        return Arrays.asList(new Account());
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Account getAccounts(@PathVariable long id){
+        return new Account();
+    }
 }
